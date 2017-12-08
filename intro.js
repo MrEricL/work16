d1e6af2fdc4b7fe1b72142714582d47f3392a95b
MrEricL <eli6@stuy.edu>
2017-12-08 15:23:52 -0500

    first func

--- intro.js
+++ intro.js
@@ -1,8 +1,13 @@
+// List
 var a = document.getElementById('thelist');
 
-a.addEventListener('click', add_to_list);
+//Button
 
-var add_to_list = function(){
+
+b.addEventListener('click', add_to_list);
+//a.addEventListener('mouseover', )
+
+function add_to_list (){
     var node = document.createElement('LI');
     var textnode = document.createTextNode('item');
     node.appendChild(textnode);
